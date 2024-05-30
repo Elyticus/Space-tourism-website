60c9d054b75c9654d7ef2d78a267d0ac73d4996a
Elyticus <125762622+Elyticus@users.noreply.github.com>
2024-05-30 16:01:19 +0300

    updates

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const navbarEl = document.getElementById("navbar");
 const menuMobileEl = document.getElementById("menuMobileIcon");
 const navbarMenuEl = document.getElementById("navbarMenuEl");
 const closeMenuBtn = document.getElementById("closeMenuBtn");
+const bulletCrewFilter = document.querySelectorAll(".bullet_crew");
 
 menuMobileEl.addEventListener("click", () => {
   navbarMenuEl.classList.toggle("navbar_menu");
@@ -9,4 +10,10 @@ menuMobileEl.addEventListener("click", () => {
 
 closeMenuBtn.addEventListener("click", () => {
   navbarMenuEl.classList.toggle("navbar_menu");
+});
+
+bulletCrewFilter.forEach((bullet) => {
+  bullet.addEventListener("click", (e) => {
+    bullet.classList.add("active");
+  });
 });
